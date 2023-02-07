ec88c0ee9a1a6a2630910c4e7ccb1daf4daeab1f
Lee Salminen <leesalminen@gmail.com>
2023-02-07 13:16:38 -0600

    update to reflect

--- index.js
+++ index.js
@@ -27,8 +27,17 @@ app.post('/order', async (req, res) => {
   const fiatAmount = (req.body.fiatAmount ? parseFloat(req.body.fiatAmount.replace(/,/g, "")) : null)
   const fiatCurrency = (req.body.fiatCurrency ? req.body.fiatCurrency.toUpperCase() : null)
   const satAmount  = parseInt(req.body.satAmount)
-  const direction  = (req.body.direction ? req.body.direction.toUpperCase() : null)
-  const paymentReq = (req.body.paymentReq ? req.body.paymentReq : null)
+  const action  = (req.body.action ? req.body.action.toUpperCase() : null)
+  let paymentReq = (req.body.paymentReq ? req.body.paymentReq : null)
+  
+  const billerCategory = req.body.billerCategory
+  const billerService = req.body.billerService
+  const billerActionType = req.body.billerActionType
+  const billerAccountNumber = req.body.billerAccountNumber
+
+  if(action === 'BILLPAY') {
+    paymentReq = 'Bill Payment'
+  }
 
   if(!apiKey) {
     return res.send({error: true, message: "apiKey is required"})
@@ -54,26 +63,74 @@ app.post('/order', async (req, res) => {
     return res.send({error: true, message: "satAmount is required"})
   }
 
-  if(!direction) {
-    return res.send({error: true, message: "direction is required"})
+  if(!action) {
+    return res.send({error: true, message: "action is required"})
   }
 
   if(!paymentReq) {
     return res.send({error: true, message: "paymentReq is required"})
   }
 
-  if(direction !== 'BUY' && direction !== 'SELL') {
-    return res.send({error: true, message: "direction should be buy or sell"})
+  if(action !== 'BUY' && action !== 'SELL' && action !== 'BILLPAY') {
+    return res.send({error: true, message: "action should be buy or sell or billpay"})
   }
 
-  if(direction === 'BUY' && paymentReq.toLowerCase().indexOf('lnbc') !== 0) {
+  if(action === 'BUY' && paymentReq.toLowerCase().indexOf('lnbc') !== 0) {
     return res.send({error: true, message: "paymentReq must start with lnbc when buying"})
+  }
+
+  if(action === 'BILLPAY' && (!billerCategory || !billerService || !billerActionType || !billerAccountNumber)) {
+    return res.send({error: true, message: "When action is BILLPAY, you must provide billerCategory, billerService, billerActionType, billerAccountNumber"})
   }
 
   const fiatFormatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: fiatCurrency,
   })
+
+  const satAmountFormatted = Number(satAmount).toLocaleString()
+  const fiatAmountFormatted = Number(fiatAmount).toLocaleString()
+
+  let fromCurrency = null
+  let fromAmount = null
+  let toCurrency = null
+  let toAmount = null
+  let paymentType = null
+  let paymentDestination = null
+  let formulaFreeAmount = (satAmount / 100000000).toFixed(8)
+
+  switch(action) {
+    case 'BUY':
+      fromCurrency = fiatCurrency
+      fromAmount = fiatAmountFormatted
+
+      toCurrency = 'BTC'
+      toAmount = `=(${satAmount} / 100000000)`
+
+      paymentType = 'Buy BTC'
+
+      paymentDestination = paymentReq
+      
+      break;
+
+    case 'SELL':
+    case 'BILLPAY':
+      fromCurrency = 'BTC'
+      fromAmount = `=(${satAmount} / 100000000)`
+
+      toCurrency = fiatCurrency
+      toAmount = fiatAmountFormatted
+
+      paymentType = 'Sell BTC'
+
+      paymentDestination = paymentReq
+
+      break;
+
+    default:
+      console.log('no buy nor sell uh-oh')
+      break;
+  }
 
   await doc.useServiceAccountAuth({
     client_email: service_account_email,
@@ -84,28 +141,35 @@ app.post('/order', async (req, res) => {
 
   const sheet = doc.sheetsByIndex[0]
 
-  const newRow = await sheet.addRow({ 
-    "Type": "TEST",
-    "From currency": "TEST",
-    "From amount": "TEST",
-    "To Currency": "TEST",
-    "TO AMOUNT": "TEST",
-    "Payment type": "TEST",
-    "Biller": "TEST",
-    "Account Number": "TEST",
-    "LN Invoice": "TEST",
-    "USD/CRC": "TEST",
-    "CRC/USD": "TEST",
-    "USD/CAD": "TEST",
-    "EFFECTIVE RATE (TO CURRENCY)": "TEST",
-    "USD RATE": "TEST",
-    "CAD RATE": "TEST",
-  })
+  const rowData = { 
+    "Type": action,
+    "From Currency": fromCurrency,
+    "From Amount": fromAmount,
+    "To Currency": toCurrency,
+    "To Amount": toAmount,
+    "Payment Type": paymentType,
+    "Payment Destination": paymentDestination,
+    "Biller Category": billerCategory,
+    "Biller Service": billerService,
+    "Biller Action Type": billerActionType,
+    "Biller Account Number": billerAccountNumber,
+  }
 
-  let message = `🚨 ${direction} Order 🚨\n`
-  message += `Fiat Amount: ${fiatFormatter.format(fiatAmount)}\n`
-  message += `Sat Amount: ${Number(satAmount).toLocaleString()}\n`
-  message += `Payment Destination: ${paymentReq}`
+  const newRow = await sheet.addRow(rowData)
+
+  let message = `🚨 New Order 🚨\n`
+
+  Object.keys(rowData).forEach(key => {
+    let val = rowData[key]
+
+    if(val && val.length) {
+      if(val[0] === '=') {
+        val = formulaFreeAmount
+      }
+
+      message += `${key}: ${val}\n`
+    }
+  })
 
   const resp = await bot.telegram.sendMessage(chat_id, message)
 
