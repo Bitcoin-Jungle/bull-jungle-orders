437421b587b71fb0d0f3ecff5ff520ca0fa030c2
Lee Salminen <leesalminen@gmail.com>
2023-02-07 11:03:31 -0600

    formatting

--- index.js
+++ index.js
@@ -25,77 +25,77 @@ const db = await open({
 app.use(bodyParser.json())
 
 app.post('/order', async (req, res) => {
-	const apiKey = req.body.apiKey
-	const fiatAmount = parseFloat(req.body.fiatAmount.replace(/,/g, ""))
-	const fiatCurrency = (req.body.fiatCurrency ? req.body.fiatCurrency.toUpperCase() : null)
-	const satAmount  = parseInt(req.body.satAmount)
-	const direction  = (req.body.direction ? req.body.direction.toUpperCase() : null)
-	const paymentReq = (req.body.paymentReq ? req.body.paymentReq : null)
+  const apiKey = req.body.apiKey
+  const fiatAmount = parseFloat(req.body.fiatAmount.replace(/,/g, ""))
+  const fiatCurrency = (req.body.fiatCurrency ? req.body.fiatCurrency.toUpperCase() : null)
+  const satAmount  = parseInt(req.body.satAmount)
+  const direction  = (req.body.direction ? req.body.direction.toUpperCase() : null)
+  const paymentReq = (req.body.paymentReq ? req.body.paymentReq : null)
 
-	if(!apiKey) {
-		return res.send({error: true, message: "apiKey is required"})
-	}
+  if(!apiKey) {
+    return res.send({error: true, message: "apiKey is required"})
+  }
 
-	if(apiKey !== api_key) {
-		return res.send({error: true, message: "apiKey is incorrect"})
-	}
+  if(apiKey !== api_key) {
+    return res.send({error: true, message: "apiKey is incorrect"})
+  }
 
-	if(!fiatAmount) {
-		return res.send({error: true, message: "fiatAmount is required"})
-	}
+  if(!fiatAmount) {
+    return res.send({error: true, message: "fiatAmount is required"})
+  }
 
-	if(!fiatCurrency) {
-		return res.send({error: true, message: "fiatCurrency is required"})
-	}
+  if(!fiatCurrency) {
+    return res.send({error: true, message: "fiatCurrency is required"})
+  }
 
-	if(fiatCurrency !== 'USD' && fiatCurrency !== 'CRC') {
-		return res.send({error: true, message: "fiatCurrency must be USD or CRC"})
-	}
+  if(fiatCurrency !== 'USD' && fiatCurrency !== 'CRC') {
+    return res.send({error: true, message: "fiatCurrency must be USD or CRC"})
+  }
 
-	if(!satAmount) {
-		return res.send({error: true, message: "satAmount is required"})
-	}
+  if(!satAmount) {
+    return res.send({error: true, message: "satAmount is required"})
+  }
 
-	if(!direction) {
-		return res.send({error: true, message: "direction is required"})
-	}
+  if(!direction) {
+    return res.send({error: true, message: "direction is required"})
+  }
 
-	if(!paymentReq) {
-		return res.send({error: true, message: "paymentReq is required"})
-	}
+  if(!paymentReq) {
+    return res.send({error: true, message: "paymentReq is required"})
+  }
 
-	if(direction !== 'BUY' && direction !== 'SELL') {
-		return res.send({error: true, message: "direction should be buy or sell"})
-	}
+  if(direction !== 'BUY' && direction !== 'SELL') {
+    return res.send({error: true, message: "direction should be buy or sell"})
+  }
 
-	if(direction === 'BUY' && paymentReq.toLowerCase().indexOf('lnbc') !== 0) {
-		return res.send({error: true, message: "paymentReq must start with lnbc when buying"})
-	}
+  if(direction === 'BUY' && paymentReq.toLowerCase().indexOf('lnbc') !== 0) {
+    return res.send({error: true, message: "paymentReq must start with lnbc when buying"})
+  }
 
-	const fiatFormatter = new Intl.NumberFormat('en-US', {
-	  style: 'currency',
-	  currency: fiatCurrency,
-	})
+  const fiatFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: fiatCurrency,
+  })
 
-	const order = await db.run(
-      "INSERT INTO orders (fiat_amount, fiat_currency, sat_amount, direction, payment_request) VALUES (?, ?, ?, ?, ?)", 
-      [
-        parseInt(fiatAmount * 100),
-        fiatCurrency,
-        satAmount,
-        direction,
-        paymentReq,
+  const order = await db.run(
+    "INSERT INTO orders (fiat_amount, fiat_currency, sat_amount, direction, payment_request) VALUES (?, ?, ?, ?, ?)", 
+    [
+      parseInt(fiatAmount * 100),
+      fiatCurrency,
+      satAmount,
+      direction,
+      paymentReq,
       ]
     )
 
-	let message = `🚨 ${direction} Order 🚨\n`
-	message += `Fiat Amount: ${fiatFormatter.format(fiatAmount)}\n`
-	message += `Sat Amount: ${Number(satAmount).toLocaleString()}\n`
-	message += `Payment Destination: ${paymentReq}`
+  let message = `🚨 ${direction} Order 🚨\n`
+  message += `Fiat Amount: ${fiatFormatter.format(fiatAmount)}\n`
+  message += `Sat Amount: ${Number(satAmount).toLocaleString()}\n`
+  message += `Payment Destination: ${paymentReq}`
 
-  	const resp = await bot.telegram.sendMessage(chat_id, message)
+  const resp = await bot.telegram.sendMessage(chat_id, message)
 
-  	res.send({success: true})
+  res.send({success: true})
 })
 
 app.get('/order', (req, res) => {
