c770262233ea9087f1e66b8f1ddfd3ad7571ac8c
Lee Salminen <leesalminen@gmail.com>
2023-02-10 16:35:43 -0600

    dont need this anymore

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -6,36 +6,15 @@ import {
   ApolloProvider,
   ApolloClient,
   InMemoryCache,
-  split,
   HttpLink,
 } from "@apollo/client"
-import { WebSocketLink } from "@apollo/client/link/ws"
-import { getMainDefinition } from "@apollo/client/utilities"
 
 const httpLink = new HttpLink({
   uri: "https://api.mainnet.bitcoinjungle.app/graphql",
 })
 
-const wsLink = new WebSocketLink({
-  uri: "wss://api.mainnet.bitcoinjungle.app/graphql",
-  options: {
-    reconnect: true,
-  },
-})
-
-const splitLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query)
-    return (
-      definition.kind === "OperationDefinition" && definition.operation === "subscription"
-    )
-  },
-  wsLink,
-  httpLink,
-)
-
 const client = new ApolloClient({
-  link: splitLink,
+  link: httpLink,
   cache: new InMemoryCache(),
 })
 
