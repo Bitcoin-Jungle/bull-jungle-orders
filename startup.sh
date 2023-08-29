a50ffabe715b0e428a5e99ef8363e3fadf0ed702
Lee Salminen <leesalminen@gmail.com>
2023-08-29 10:16:37 -0600

    try this

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Start the first process
+node index.js &
+
+# Start the second process
+tor -f /etc/torrc &
+
+# Wait for any process to exit
+wait -n
+
+# Exit with status of process that exited first
+exit $?
